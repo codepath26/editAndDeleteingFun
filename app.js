f29a39f61800b3c79874a35c874866174a790266
parth thakor <parththakor2610@gmail.com>
2023-08-17 13:54:11 +0530

    all implimentation related to sequelize

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database') //importing the database to our applications
+const sequelize = require('./util/database') //importing the database to our applications
 
 const app = express();
 
@@ -13,6 +13,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { serialize } = require('v8');
 // db.execute('SELECT * FROM products').then((result)=>{console.log(result[0] , result[1]);}).catch((err)=>{console.log(err);})
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,4 +23,9 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(5000);
+sequelize.sync().then((res)=>{
+  // console.log(res)
+  app.listen(5000);
+}).catch(err=>console.log(err))
+
+
--- models/product.js
+++ models/product.js
@@ -1,27 +1,26 @@
-const Cart = require('./cart');
-const db = require('../util/database')
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const Sequelize = require("sequelize")
+const sequelize = require('../util/database')
 
-  save() {
-   return db.execute('INSERT INTO products (title,price,description,imageUrl) VALUES (?,?,?,?)',[this.title,this.price,this.description,this.imageUrl]);
-    
+const Product = sequelize.define('product' , {
+  id :{
+    type : Sequelize.INTEGER,
+    autoIncrement : true ,
+    allowNull : false,
+    primaryKey : true
+  },
+  title : Sequelize.STRING,
+  price :{
+    type : Sequelize.DOUBLE,
+    allowNull : false
+  },
+  imageUrl :{
+  type : Sequelize.STRING,
+  allowNull : false
+  },
+  description :{
+  type : Sequelize.STRING,
+  allowNull  : false
   }
-  static deleteById(id) {
-    return db.execute('DELETE FROM products WHERE products.id =?',[id]);
-  }
-  static fetchAll(cb) {
-   return db.execute('SELECT * FROM products');
-  }
+})
 
-  static findById(id, cb) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?',[id]);
-   
-  }
-};
+module.exports = Product ;
