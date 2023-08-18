d640ea40623b3092eda3a3f5271b7e65532bd46d
parth thakor <parththakor2610@gmail.com>
2023-08-18 06:19:42 +0530

    this is updated using the database deleteand edit functionality

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database') //importing the database to our applications
-
+const sequelize = require('./util/database') 
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -14,18 +13,41 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const { serialize } = require('v8');
+const User = require('./models/user');
+const Product = require('./models/product');
 // db.execute('SELECT * FROM products').then((result)=>{console.log(result[0] , result[1]);}).catch((err)=>{console.log(err);})
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use((req ,res,next) => {
+  User.findByPk(1).then(
+    user =>{
+      req.user = user;
+      next();
+    }
+  ).catch(err=>console.log(err));
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+Product.belongsTo(User, {constrains : true, onDelete  : "CASCADE"});
+User.hasMany(Product);
 
 sequelize.sync().then((res)=>{
-  // console.log(res)
-  app.listen(5000);
-}).catch(err=>console.log(err))
+  return User.findByPk(1)
+
+}).then((user)=>{
+  if(!user){
+    return  User.create({name : "Rocky" , email : 'useremain@gmail.com'})
+  }
+  return user;
+  // return Promise.resolve(user); but we are in then block so that return result automatically convert into a promiss
+  
+}).then((user)=>{
+  // console.log(user);
+  // app.listen(5000);
+app.listen(3000);
+})
+.catch(err=>console.log(err))
 
 
--- models/product.js
+++ models/product.js
@@ -1,26 +1,27 @@
-const Sequelize = require("sequelize")
-const sequelize = require('../util/database')
+const { Sequelize,DataTypes } = require('sequelize');
+const sequelize = require('../util/database');
 
-const Product = sequelize.define('product' , {
-  id :{
-    type : Sequelize.INTEGER,
-    autoIncrement : true ,
-    allowNull : false,
-    primaryKey : true
-  },
-  title : Sequelize.STRING,
-  price :{
-    type : Sequelize.DOUBLE,
-    allowNull : false
-  },
-  imageUrl :{
-  type : Sequelize.STRING,
-  allowNull : false
-  },
-  description :{
-  type : Sequelize.STRING,
-  allowNull  : false
+const Product = sequelize.define(
+  'products',{
+    id :{
+      type : Sequelize.DataTypes.INTEGER,
+      autoIncrement : true,
+      allowNull : false,
+      primaryKey : true
+    },
+    title : Sequelize.DataTypes.STRING,
+    price : {
+     type : Sequelize.DataTypes.DOUBLE,
+     allowNull : false  
+    },
+    imageUrl : {
+      type : Sequelize.STRING,
+      allowNull : false
+    },
+    description : {
+      type : Sequelize.DataTypes.STRING,
+      allowNull : false
+    }
   }
-})
-
+) 
 module.exports = Product ;
